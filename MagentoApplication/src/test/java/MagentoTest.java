7d1045e0927cc09f04d4c49e945ab0cd89c92337
sarkprad <sarkprad@LACRB41ZWL.asiapacific.hpqcorp.net>
2019-02-03 12:44:49 +0530

    Firefox driver changed to in MagentoTest.java

--- MagentoApplication/src/test/java/MagentoTest.java
+++ MagentoApplication/src/test/java/MagentoTest.java
@@ -2,7 +2,7 @@
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class MagentoTest {
@@ -12,7 +12,7 @@ public class MagentoTest {
 
 		String url = "http:\\www.magento.com";
 
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.get(url);
